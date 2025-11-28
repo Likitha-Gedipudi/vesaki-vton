7e1bf90e20c61007aa043401efb8f4b2ee8d0093
LikithaG <likig@Likithas-MacBook-Air.local>
2025-11-27 23:21:21 -0500

    fix: Export TryOnModuleAdvanced in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,16 +7,20 @@ This package contains the neural network architectures for virtual try-on:
 - Supporting networks and utilities
 """
 
-from .gmm import GeometricMatchingModule
-from .tom import TryOnModule
+from .gmm import GeometricMatchingModule, GMMWithRefinement
+from .tom import TryOnModule, TryOnModuleAdvanced, CompleteTryOnModel
 from .networks import FeatureExtraction, FeaturePyramid
-from .vgg import VGGLoss
+from .vgg import VGGLoss, VGG19
 
 __all__ = [
     'GeometricMatchingModule',
+    'GMMWithRefinement',
     'TryOnModule',
+    'TryOnModuleAdvanced',
+    'CompleteTryOnModel',
     'FeatureExtraction',
     'FeaturePyramid',
-    'VGGLoss'
+    'VGGLoss',
+    'VGG19'
 ]
 
