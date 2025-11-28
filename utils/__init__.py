4a034d65592fd2d903fc845bf4cd0c1d4d9bb4e4
LikithaG <likig@Likithas-MacBook-Air.local>
2025-11-27 23:22:23 -0500

    fix: Add visualize_batch to utils exports
    All training script imports now working correctly.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,7 @@ Utility functions for Vesaki-VTON
 """
 
 from .metrics import compute_ssim, compute_psnr, compute_fid, compute_lpips
-from .visualization import save_images, tensor_to_image
+from .visualization import save_images, tensor_to_image, visualize_batch
 from .checkpoints import save_checkpoint, load_checkpoint
 
 __all__ = [
@@ -13,6 +13,7 @@ __all__ = [
     'compute_lpips',
     'save_images',
     'tensor_to_image',
+    'visualize_batch',
     'save_checkpoint',
     'load_checkpoint'
 ]
